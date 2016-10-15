47250646712ba0e8bbb185967949e13f372fce97
Vitaly Domnikov <dotcypress@gmail.com>
2016-10-15 02:31:37 -0700

    bypass vm options

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function escape (template) {
   return `\`${template.replace(regex, '\\`')}\``
 }
 
-function compile (template, defaultContext) {
+function compile (template, defaultContext, options) {
   if (typeof template !== 'string') {
     throw new Error('Template must be string')
   }
   const script = new vm.Script(escape(template))
-  return (context) => script.runInNewContext(Object.assign({}, defaultContext, context))
+  return (context) => script.runInNewContext(Object.assign({}, defaultContext, context), options)
 }
 
 module.exports = compile
